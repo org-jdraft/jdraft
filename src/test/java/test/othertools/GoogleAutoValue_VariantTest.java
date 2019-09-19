01602d614ef51e10d6ddea175eed856d3a38ebfe
Eric DeFazio <medefazio@yahoo.com>
2019-09-19 16:16:39 -0400

    added new tests to compare/contrast the API with jdraft API

---
+++ src/test/java/test/othertools/GoogleAutoValue_VariantTest.java
@@ -0,0 +1,87 @@
+package test.othertools;
+
+import com.github.javaparser.ast.body.TypeDeclaration;
+import junit.framework.TestCase;
+import org.jdraft._class;
+import org.jdraft._type;
+import org.jdraft.macro._final;
+import org.jdraft.macro._macro;
+import org.jdraft.proto.$;
+import org.jdraft.proto.$field;
+import org.jdraft.proto.$method;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.function.Consumer;
+
+public class GoogleAutoValue_VariantTest extends TestCase {
+
+    /**
+     * There are a few other variants o AutoValue to cover:
+     * https://github.com/google/auto/blob/master/value/userguide/builders-howto.md#-use-or-not-use-set-prefixes
+     */
+    public void testNoGetSetPrefixes(){
+
+        @_dto_NoSetPrefix class C{
+            int a;
+            @_final int b;
+        }
+        _class _c = _class.of(C.class);
+        System.out.println( _c);
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ElementType.TYPE, ElementType.TYPE_USE})
+    @interface _dto_NoSetPrefix{
+
+        public static Consumer<TypeDeclaration> C = new Act();
+
+        /*
+        public static _macro<_type> $THIN = new _macro<_type>(){
+
+            @Override
+            public _type apply(_type type) {
+                System.out.println( "Doing it");
+                Act.act(type);
+                return type;
+            }
+        };
+         */
+
+        class Act implements Consumer<TypeDeclaration>{
+
+
+            public static final $method $setNoPrefix = $method.of("public void $name$($type$ $name$){ this.$name$ = $name$; }");
+            public static final $field $setterFields = $field.of($.NOT_STATIC, $.NOT_FINAL);
+
+            public Act( ){
+                System.out.println( "In ctor");
+            }
+
+            public Act(_dto_NoSetPrefix d){
+                //
+                System.out.println( "In ctor");
+            }
+
+            @Override
+            public void accept(TypeDeclaration typeDeclaration) {
+                $setterFields.forEachIn(typeDeclaration, f-> typeDeclaration.addMember( $setNoPrefix.fill(f.getName(), f.getType()).ast() ) );
+                //act( _java.type(typeDeclaration));
+
+            }
+
+            public static void act( _type _t ){
+                //if( _t instanceof ){
+                $.field($.NOT_STATIC, $.NOT_FINAL).forEachIn(_t, f-> _t.add( $setNoPrefix.fill(f.getName(), f.getType())));
+
+                //_t.listFields( (f) -> !((_field)f).isFinal() && !((_field)f).isStatic());
+                //typeDeclaration.getFields().stream().filter(f-> !f.)
+                //_toString.Act.to(typeDeclaration);
+                //_equals.Act.to(typeDeclaration);
+                //_hashCode.Act.to(typeDeclaration);
+            }
+        }
+    }
+}
---
+++ src/test/java/test/othertools/SemmleLGTMQueryTests.java
@@ -0,0 +1,122 @@
+package test.othertools;
+
+import com.github.javaparser.ast.body.FieldDeclaration;
+import junit.framework.TestCase;
+import org.jdraft.Ast;
+import org.jdraft.Stmt;
+import org.jdraft._typeRef;
+import org.jdraft.proto.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * https://blog.semmle.com/secure-software-github-semmle/
+ *
+ */
+public class SemmleLGTMQueryTests extends TestCase {
+
+    /**
+     * https://lgtm.com/query
+     *
+     * Semmle LGTM query:
+     * Basic query: Array access
+     * Finds array access expressions with an index expression consisting of a unary assignment.
+     *
+     * from ArrayAccess a
+     * where a.getIndexExpr() instanceof UnaryAssignExpr
+     * select a
+     *
+     */
+    public void testQueryArrayAccessIntoUnaryExpression(){
+        $ex $e = $ex.arrayAccessEx().$hasChild( $ex.unaryEx() );
+        //now use it
+        assertNotNull($e.firstIn( Stmt.of("$buffer[position++] = (byte)(value >>> 8);") ));
+    }
+
+    /**
+     * Semmle LGTM query <A HREF="https://lgtm.com/query/">online</A>:
+     * import java
+     *
+     * from Method m
+     * where m.getReturnType() instanceof VoidType
+     * select m
+     *
+     */
+    public void testQueryReturnTypeVoid(){
+        $method $m = $method.of( $typeRef.VOID );
+        class C{
+            void m(){ }
+        }
+        assertNotNull($m.firstIn(C.class));
+    }
+
+    /**
+     * import java
+     *
+     * from ReturnStmt r
+     * where r.getResult() instanceof NullLiteral
+     * select r
+     */
+    public void testReturnNullStatements(){
+        $stmt $s = $$.returnStmt("return null;");
+        class C{
+            Object oo(){
+                return null;
+            }
+        }
+        assertNotNull($s.firstIn(C.class));
+    }
+
+    /**
+     * TODO I can make this better
+     *
+     */
+    public void testAContainerNeverUsed(){
+        //TODO make $typeRef constructor for many Classes
+        $typeRef $containerType = $typeRef.of(List.class);
+
+
+        //another
+
+        //an outside access to the field
+        $ex $outsideFieldAccess = $$.fieldAccessExpr("$className$.$fieldName$");
+
+        //$reflectivelyRead ??
+
+        //omit private fields, & fields with
+        //$field means only (member)fields (not $vars or $params)
+        $field $f = $field.of( $containerType ) //all fields that are "containers" (i.e. collections, list, map, HashSet, LinkedList)
+                .$not(
+                        $.PRIVATE,
+                        $anno.of("@SuppressWarnings(\"unused\")"),
+                        $anno.of("@lombok.Getter") //could make this "better" by passing in Lombok class
+                );
+                //and the parent isnt a private
+                //.$not(v-> Ast.isParent(v, FieldDeclaration.class) ); //a local var (not a field or parameter)
+
+        class SomeClass{
+            List v = new ArrayList<>();
+            private List l = new ArrayList();
+            @SuppressWarnings("unused")
+            List t = new ArrayList();
+
+            void some(){
+                v.get(0);
+            }
+        }
+        //all methodCalls with scope
+        $$.expr("v").printIn(SomeClass.class);
+        //$$.methodCall().$and(m -> m.getScope().isPresent() && m.getScope().get().).printIn(SomeClass.class);
+
+        //gets the list of all container classes
+        //$f.printIn(SomeClass.class);
+
+        //$field.of($typeRef.of(List.class)).$not($.PRIVATE).printIn(SomeClass.class);
+        //$field.of($typeRef.of(List.class)).$not($.PRIVATE, $anno.of("Getter")).printIn(SomeClass.class);
+        $field.of($typeRef.of(List.class)).$not($.PRIVATE, $anno.of("@SuppressWarnings(\"unused\")")).printIn(SomeClass.class);
+        //$field.of( $containerType )
+    }
+}
