ea2d8c11678504013450993df488e7d186073606
Eric DeFazio <medefazio@yahoo.com>
2019-11-02 20:29:23 -0400

    $anno.or
    and tests

--- src/main/java/org/jdraft/_draft.java
+++ src/main/java/org/jdraft/_draft.java
@@ -3,7 +3,8 @@ package org.jdraft;
 /**
  * Marker interface for all Java _meta model entity implementations
  * (We have this interface to differentiate between entities/classes that are
- * Ast entities (i.e. Node) and
+ * Ast entities (i.e. Node) and part of the _jdraft API which sits on top of
+ * the JavaParser Ast nodes
  */
 public interface _draft {
 
