bd2d90b7b0a4c537ddf004ad2501c8cd2e0b6207
Eric DeFazio <medefazio@yahoo.com>
2020-02-22 11:51:27 -0500

    added more API features specifically getX setX forX isX
    methods for fields of _expressions and _statement
    entities

--- src/test/java/org/jdraft/pattern/StypeParameterTest.java
+++ src/test/java/org/jdraft/pattern/StypeParameterTest.java
@@ -120,7 +120,7 @@ public class StypeParameterTest extends TestCase {
             int a;
             String b;
 
-            public <A extends Map & _java._compoundNode> A m() {
+            public <A extends Map & _java._multiPart> A m() {
                 return null;
             }
         }
