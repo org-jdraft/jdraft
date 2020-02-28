5b77d68a34232579b72e86ea9d8b6aaa1c574812
Eric DeFazio <medefazio@yahoo.com>
2020-02-27 19:40:33 -0500

    updated api for _variable

--- src/main/java/org/jdraft/_variable.java
+++ src/main/java/org/jdraft/_variable.java
@@ -1,6 +1,8 @@
 package org.jdraft;
 
 import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.VariableDeclarationExpr;
 
 import java.util.Objects;
 
@@ -41,6 +43,112 @@ public class _variable implements _java._astNode<VariableDeclarator, _variable>,
             return Objects.equals( this.vd, _v.ast());
         }
         return false;
+    }
+
+    public boolean isFinal(){
+        if( this.vd.getParentNode().isPresent() &&  this.vd.getParentNode().get() instanceof VariableDeclarationExpr){
+            //check if the parent node describing this is
+            VariableDeclarationExpr vde = (VariableDeclarationExpr) this.vd.getParentNode().get();
+            return vde.isFinal();
+        }
+        return false;
+    }
+
+    public _expression getInit(){
+        if( this.vd.getInitializer().isPresent()){
+            return _expression.of( vd.getInitializer().get());
+        }
+        return null;
+    }
+
+
+    public _variable setInit(String...e){
+        this.vd.setInitializer(Ex.of(e));
+        return this;
+    }
+
+    public _variable setInit(Expression e){
+        this.vd.setInitializer(e);
+        return this;
+    }
+
+    public _variable setInit(_expression _e){
+        this.vd.setInitializer(_e.ast());
+        return this;
+    }
+
+    public _variable removeInit(){
+        this.vd.removeInitializer();
+        return this;
+    }
+
+    public _variable setInit(boolean b){
+        return setInit(Ex.of(b));
+    }
+
+    public _variable setInit(int i){
+        return setInit(Ex.of(i));
+    }
+
+    public _variable setInit(char c){
+        return setInit(Ex.of(c));
+    }
+
+    public _variable setInit(float f){
+        return setInit(Ex.of(f));
+    }
+
+    public _variable setInit(long l){
+        return setInit(Ex.of(l));
+    }
+
+    public _variable setInit(double d){
+        return setInit(Ex.of(d));
+    }
+
+
+    public boolean hasInit(){
+        return this.vd.getInitializer().isPresent();
+    }
+
+    public boolean isInit(boolean b){
+        return isInit(Ex.of(b));
+    }
+
+    public boolean isInit(int i){
+        return isInit(Ex.of(i));
+    }
+
+    public boolean isInit(char c){
+        return isInit(Ex.of(c));
+    }
+
+    public boolean isInit(float f){
+        return isInit(Ex.of(f));
+    }
+
+    public boolean isInit(long l){
+        return isInit(Ex.of(l));
+    }
+
+    public boolean isInit(double d){
+        return isInit(Ex.of(d));
+    }
+
+    public boolean isInit(String...ex){
+        try{
+            return isInit(Ex.of(ex));
+        }catch(Exception e){
+            return false;
+        }
+    }
+    public boolean isInit(_expression _e){
+        return isInit(_e.ast());
+    }
+
+    public boolean isInit(Expression e){
+        return this.vd.getInitializer().isPresent() &&
+                Ex.equivalent( this.vd.getInitializer().get(), e);
     }
 
     public int hashCode( ){
--- src/test/java/org/jdraft/_variableTest.java
+++ src/test/java/org/jdraft/_variableTest.java
@@ -8,5 +8,21 @@ public class _variableTest extends TestCase {
         _variable _v = _variable.of("int i");
         assertTrue(_v.isNamed("i"));
         assertTrue(_v.isType(int.class));
+        assertFalse( _v.hasInit());
+        assertFalse( _v.isFinal());
+
+        //now init
+        _v.setInit(1);
+        assertTrue( _v.hasInit());
+        assertTrue( _v.isInit(1));
+        //remove init
+        _v.removeInit();
+        assertFalse( _v.hasInit());
+        assertFalse( _v.isInit(1));
+
+        _v.setName("newName");
+        assertEquals("newName", _v.getName());
+
     }
+
 }
